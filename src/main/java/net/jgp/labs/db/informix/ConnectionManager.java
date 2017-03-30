af53e65003641840b0e2f78d06fb8056483c659e
Jean Georges Perrin <jgp@jgp.net>
2017-03-30 17:29:14 -0400

    More flexible.

--- src/main/java/net/jgp/labs/db/informix/ConnectionManager.java
+++ src/main/java/net/jgp/labs/db/informix/ConnectionManager.java
@@ -12,13 +12,17 @@ public class ConnectionManager {
 	private String hostname = "[::1]";
 
 	private int port = 33378;
-	
+
 	private String user = "informix";
-	
+
 	private String password = "in4mix";
 
+	private String database = "stores_demo";
+
+	private String informixServer = "lo_informix1210";
+
 	private static ConnectionManager instance = null;
-	
+
 	public static Connection getConnection() {
 		ConnectionManager cm = ConnectionManager.getInstance();
 		return cm.getConnection0();
@@ -35,8 +39,8 @@ public class ConnectionManager {
 		}
 
 		// Setup the connection with the DB
-		String jdbcUrl = "jdbc:informix-sqli://" + hostname + ":" + port
-				+ "/stores_demo:INFORMIXSERVER=lo_informix1210";
+		String jdbcUrl = "jdbc:informix-sqli://" + hostname + ":" + port + "/" + database + ":INFORMIXSERVER="
+				+ informixServer;
 		try {
 			connect = DriverManager.getConnection(jdbcUrl, user, password);
 		} catch (SQLException e) {
