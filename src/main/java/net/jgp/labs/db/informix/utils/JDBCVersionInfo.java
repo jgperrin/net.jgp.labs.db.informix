e2cbc07635d8079633076d934fecee1d75c2d190
Jean Georges Perrin <jgp@jgp.net>
2017-04-13 10:05:54 -0400

    Added example with all the details of the JDBC code.

--- src/main/java/net/jgp/labs/db/informix/utils/JDBCVersionInfo.java
+++ src/main/java/net/jgp/labs/db/informix/utils/JDBCVersionInfo.java
@@ -2,21 +2,42 @@ package net.jgp.labs.db.informix.utils;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCVersionInfo {
 
 	public static void main(String[] args) {
-		Connection connection = ConnectionManager.getConnection();
-		if (connection == null) {
-			System.out.println("Could not get a connection to the database.");
+
+		try {
+			Class.forName("com.informix.jdbc.IfxDriver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Could not access JDBC driver's class: " + e.getMessage());
 			return;
 		}
+
+		String hostname = "[::1]";
+		int port = 33378;
+		String user = "informix";
+		String password = "in4mix";
+		String database = "stores_demo";
+		String informixServer = "lo_informix1210";
+		Connection connection;
+
+		String jdbcUrl = "jdbc:informix-sqli://" + hostname + ":" + port + "/" + database + ":INFORMIXSERVER="
+				+ informixServer;
+		try {
+			connection = DriverManager.getConnection(jdbcUrl, user, password);
+		} catch (SQLException e) {
+			System.err.println("Could not get a connection to the database: " + e.getMessage());
+			return;
+		}
+
 		DatabaseMetaData dbmd;
 		try {
 			dbmd = connection.getMetaData();
 		} catch (SQLException e) {
-			System.out.println("An error occured while accessing metadata: " + e.getMessage());
+			System.err.println("An error occured while accessing metadata: " + e.getMessage());
 			return;
 		}
 
@@ -28,9 +49,7 @@ public class JDBCVersionInfo {
 			System.out.println("Database name .......... " + dbmd.getDatabaseProductName());
 			System.out.println("Database version ....... " + dbmd.getDatabaseProductVersion());
 		} catch (SQLException e) {
-			System.out.println("An error occured while extracting metadata: " + e.getMessage());
+			System.err.println("An error occured while extracting metadata: " + e.getMessage());
 		}
-
 	}
-
 }
