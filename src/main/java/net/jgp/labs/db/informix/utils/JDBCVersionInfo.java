8cd676c3b1fc5fc382f0449e04dc630a166b8279
Jean Georges Perrin <jgp@jgp.net>
2017-04-15 07:36:31 -0400

    Formatter for book

--- src/main/java/net/jgp/labs/db/informix/utils/JDBCVersionInfo.java
+++ src/main/java/net/jgp/labs/db/informix/utils/JDBCVersionInfo.java
@@ -7,49 +7,64 @@ import java.sql.SQLException;
 
 public class JDBCVersionInfo {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		try {
-			Class.forName("com.informix.jdbc.IfxDriver");
-		} catch (ClassNotFoundException e) {
-			System.err.println("Could not access JDBC driver's class: " + e.getMessage());
-			return;
-		}
+    try {
+      Class.forName("com.informix.jdbc.IfxDriver");
+    } catch (ClassNotFoundException e) {
+      System.err.println("Could not access JDBC driver's class: "
+          + e.getMessage());
+      return;
+    }
 
-		String hostname = "[::1]";
-		int port = 33378;
-		String user = "informix";
-		String password = "in4mix";
-		String database = "stores_demo";
-		String informixServer = "lo_informix1210";
-		Connection connection;
+    String hostname = "[::1]";
+    int port = 33378;
+    String user = "informix";
+    String password = "in4mix";
+    String database = "stores_demo";
+    String informixServer = "lo_informix1210";
+    Connection connection;
 
-		String jdbcUrl = "jdbc:informix-sqli://" + hostname + ":" + port + "/" + database + ":INFORMIXSERVER="
-				+ informixServer;
-		try {
-			connection = DriverManager.getConnection(jdbcUrl, user, password);
-		} catch (SQLException e) {
-			System.err.println("Could not get a connection to the database: " + e.getMessage());
-			return;
-		}
+    String jdbcUrl = "jdbc:informix-sqli://" + hostname + ":"
+        + port + "/" + database + ":INFORMIXSERVER="
+        + informixServer;
+    try {
+      connection = DriverManager.getConnection(jdbcUrl, user,
+          password);
+    } catch (SQLException e) {
+      System.err.println(
+          "Could not get a connection to the database: " + e
+              .getMessage());
+      return;
+    }
 
-		DatabaseMetaData dbmd;
-		try {
-			dbmd = connection.getMetaData();
-		} catch (SQLException e) {
-			System.err.println("An error occured while accessing metadata: " + e.getMessage());
-			return;
-		}
+    DatabaseMetaData dbmd;
+    try {
+      dbmd = connection.getMetaData();
+    } catch (SQLException e) {
+      System.err.println(
+          "An error occured while accessing metadata: " + e
+              .getMessage());
+      return;
+    }
 
-		try {
-			System.out.println("Driver name ............ " + dbmd.getDriverName());
-			System.out.println("Driver version ......... " + dbmd.getDriverVersion());
-			System.out.println("Driver major version ... " + dbmd.getDriverMajorVersion());
-			System.out.println("Driver minor version ... " + dbmd.getDriverMinorVersion());
-			System.out.println("Database name .......... " + dbmd.getDatabaseProductName());
-			System.out.println("Database version ....... " + dbmd.getDatabaseProductVersion());
-		} catch (SQLException e) {
-			System.err.println("An error occured while extracting metadata: " + e.getMessage());
-		}
-	}
+    try {
+      System.out.println("Driver name ............ " + dbmd
+          .getDriverName());
+      System.out.println("Driver version ......... " + dbmd
+          .getDriverVersion());
+      System.out.println("Driver major version ... " + dbmd
+          .getDriverMajorVersion());
+      System.out.println("Driver minor version ... " + dbmd
+          .getDriverMinorVersion());
+      System.out.println("Database name .......... " + dbmd
+          .getDatabaseProductName());
+      System.out.println("Database version ....... " + dbmd
+          .getDatabaseProductVersion());
+    } catch (SQLException e) {
+      System.err.println(
+          "An error occured while extracting metadata: " + e
+              .getMessage());
+    }
+  }
 }
